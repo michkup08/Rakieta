bb4643f8bb3f9b62021816b011fa2195e08c1fe4
michkup08 <91846445+michkup08@users.noreply.github.com>
2024-04-03 14:36:54 +0200

    Start projektu

---
+++ Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,127 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody rigidbody;
+    [Header("Input")]
+    public float accX, accY, accZ;
+    public float rotateX, rotateY, rotateZ;
+    [Header("Virables")]
+    public float movForce;
+    public float rotateSpeed;
+    public Vector3 movement, rotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        
+    }
+    void inputs()
+    {
+        //force
+        if(Input.GetKey(KeyCode.Q))
+        {
+            accX = 1;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            accX = -1;
+        }
+        else
+        {
+            accX = 0;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            accY = 1;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            accY = -1;
+        }
+        else
+        {
+            accY = 0;
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            accZ = 1;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            accZ = -1;
+        }
+        else
+        {
+            accZ = 0;
+        }
+
+        //rotate
+        if (Input.GetKey(KeyCode.R))
+        {
+            rotateX = 1;
+        }
+        else if (Input.GetKey(KeyCode.F))
+        {
+            rotateX = -1;
+        }
+        else
+        {
+            rotateX = 0;
+        }
+        if (Input.GetKey(KeyCode.T))
+        {
+            rotateY = 1;
+        }
+        else if (Input.GetKey(KeyCode.G))
+        {
+            rotateY = -1;
+        }
+        else
+        {
+            rotateY = 0;
+        }
+        if (Input.GetKey(KeyCode.Y))
+        {
+            rotateZ = 1;
+        }
+        else if (Input.GetKey(KeyCode.H))
+        {
+            rotateZ = -1;
+        }
+        else
+        {
+            rotateZ = 0;
+        }
+    }
+
+    void movementUpdate()
+    {
+        movement = new Vector3(accX, accY, accZ) * movForce;
+        rotation += new Vector3(rotateX, rotateY, rotateZ) * rotateSpeed ;
+        rotation *= (float)Math.Pow(0.95f, 60 * Time.deltaTime);
+    }
+
+    void movementExecute()
+    {
+        rigidbody.AddForce(movement, ForceMode.Acceleration);
+        rigidbody.velocity *= (float) Math.Pow(0.99f, 60 * Time.deltaTime);
+        
+        
+        transform.Rotate(rotation);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputs();
+        movementUpdate();
+        movementExecute();
+    }
+
+}
